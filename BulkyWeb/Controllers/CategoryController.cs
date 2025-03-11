c383508d06100b7f8a6c045aac4caa163ff8f2a7
Shubham Saraswat <shubhamsaraswat89055@gmail.com>
2025-03-11 16:28:23 +0530

    Add more functionality

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using BulkyWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly BulkyWebDbContext dbContext;
+
+        public CategoryController(BulkyWebDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            var objCategoryList = dbContext.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,7 +1,15 @@
+using BulkyWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<BulkyWebDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BulkyWebDefaultConnection"));
+});
 
 var app = builder.Build();
 
